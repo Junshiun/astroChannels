1c7b6c25c86b798d47a34f03b2f4520596a1a7e0
Junshiun <55488934+Junshiun@users.noreply.github.com>
2022-07-07 23:27:15 +0800

    add context provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Channels } from "./pages/channels";
 import { Channel } from "./pages/channel";
+import { Context } from "./context/context";
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter basename="channels">
-      <Routes>
-        <Route path="/" exact element={<Channels />}></Route>
-        <Route path="/:channel" element={<Channel />}></Route>
-        <Route path="*" exact element={<Navigate to="/" />} />
-      </Routes>
+    <BrowserRouter>
+      <Context>
+        <Routes>
+          <Route path="/" exact element={<Channels />}></Route>
+          <Route path="/channels/:channel" element={<Channel />}></Route>
+          <Route path="*" exact element={<Navigate to="/" />} />
+        </Routes>
+      </Context>
     </BrowserRouter>
   );
 }
