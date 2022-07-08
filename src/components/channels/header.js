22c98d6e83a7d3f82dbf0119ef84840f3778618d
Junshiun <55488934+Junshiun@users.noreply.github.com>
2022-07-08 22:35:34 +0800

    separates children components

--- src/components/channels/header.js
+++ src/components/channels/header.js
@@ -1,30 +1,8 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { BiSearch } from "react-icons/bi";
-import {
-  FaSortNumericDownAlt,
-  FaSortNumericDown,
-  FaSortAlphaDown,
-  FaSortAlphaDownAlt,
-} from "react-icons/fa";
-import { useSearchParams } from "react-router-dom";
-import { ChannelState } from "../../context/context";
-import {
-  RESET,
-  SEARCH,
-  SORT_BYNAME_ASCENDING,
-  SORT_BYNAME_DESCENDING,
-  SORT_BYNUM_ASCENDING,
-  SORT_BYNUM_DESCENDING,
-} from "../../context/reducer";
 import { ParamsState } from "../../pages/channels";
-import { Filter } from "./filter";
 import "./header.scss";
 export const Header = () => {
-  const {
-    channels: { sortType },
-    dispatchChannels,
-  } = ChannelState();
-
   const { params, setParams } = ParamsState();
 
   const [searchInput, setSearchInput] = useState(
@@ -33,28 +11,14 @@ export const Header = () => {
       : params.get("search")
   );
 
-  const sortByNumAscending = () => {
-    dispatchChannels({ type: SORT_BYNUM_ASCENDING });
-  };
-
-  const sortByNumDESCENDING = () => {
-    dispatchChannels({ type: SORT_BYNUM_DESCENDING });
-  };
-
-  const sortByNameAscending = () => {
-    dispatchChannels({ type: SORT_BYNAME_ASCENDING });
-  };
-
-  const sortByNameDESCENDING = () => {
-    dispatchChannels({ type: SORT_BYNAME_DESCENDING });
-  };
-
   const handleSearch = (e) => {
     e.preventDefault();
 
     let value = e.target.search.value === "" ? null : e.target.search.value;
 
-    setParams({ search: value, categories: params.get("categories") });
+    setParams({
+      search: value,
+    });
   };
 
   return (
@@ -72,36 +36,6 @@ export const Header = () => {
           ></input>
         </div>
       </form>
-      <div className="refineWrap">
-        <div className="sortWrap">
-          <span>sort: </span>
-          <button
-            onClick={sortByNumAscending}
-            className={sortType === SORT_BYNUM_ASCENDING ? "active" : ""}
-          >
-            <FaSortNumericDown></FaSortNumericDown>
-          </button>
-          <button
-            onClick={sortByNumDESCENDING}
-            className={sortType === SORT_BYNUM_DESCENDING ? "active" : ""}
-          >
-            <FaSortNumericDownAlt></FaSortNumericDownAlt>
-          </button>
-          <button
-            onClick={sortByNameAscending}
-            className={sortType === SORT_BYNAME_ASCENDING ? "active" : ""}
-          >
-            <FaSortAlphaDown></FaSortAlphaDown>
-          </button>
-          <button
-            onClick={sortByNameDESCENDING}
-            className={sortType === SORT_BYNAME_DESCENDING ? "active" : ""}
-          >
-            <FaSortAlphaDownAlt></FaSortAlphaDownAlt>
-          </button>
-        </div>
-        <Filter></Filter>
-      </div>
     </div>
   );
 };
