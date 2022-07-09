7e682eb21e97ee6d7730fb43e2f9a607a6bafd97
Junshiun <55488934+Junshiun@users.noreply.github.com>
2022-07-09 21:13:59 +0800

    modify routes and add header

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Channels } from "./pages/channels";
 import { Channel } from "./pages/channel";
 import { Context } from "./context/context";
+import { Header } from "./components/header/header";
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/astroChannels">
       <Context>
         <Routes>
-          <Route path="/" exact element={<Channels />}></Route>
-          <Route path="/channels/:channel" element={<Channel />}></Route>
+          <Route
+            path="/"
+            exact
+            element={
+              <Header>
+                <Channels />
+              </Header>
+            }
+          ></Route>
+          <Route
+            path="/channels/:channel"
+            element={
+              <Header>
+                <Channel />
+              </Header>
+            }
+          ></Route>
           <Route path="*" exact element={<Navigate to="/" />} />
         </Routes>
       </Context>
